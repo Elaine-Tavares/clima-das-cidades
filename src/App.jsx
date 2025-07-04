57af004593fd16eed642b9bad2c1183249616870
Elaine-Tavares <elainetavares.developer@gmail.com>
2025-07-04 17:41:49 -0300

    inclusão do spinner

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import Busca from "./components/Busca";
 import ClimaAtual from "./components/ClimaAtual";
 import Footer from "./components/Footer";
 import Previsao from "./components/Previsao";
+import Spinner from "./components/Spinner";
 
 function App() {
  const [cidade, setCidade] = useState("");
  const [clima, setClima] = useState(null);
  const [previsao, setPrevisao] = useState([]);
-//  const [hora, setHora] = useState(null)
+ const [loading, setLoading] = useState(false);
+
 
  const apiKey = import.meta.env.VITE_API_KEY || null;
   console.log("API_KEY", apiKey)
 
   useEffect(() => {
+    setLoading(true); // Início do carregamento
     navigator.geolocation.getCurrentPosition(async (position) => {
       console.log("POSITION: ", position);
 
@@ -30,11 +33,14 @@ function App() {
 
       setCidade(respostaGeoLocalizacao.data.name)
       setClima(respostaGeoLocalizacao.data)
+      setLoading(false); // Fim do carregamento
+
     });
   }, [apiKey]);
 
 const buscarClima = async () => {
   try {
+    setLoading(true); // Início do carregamento
     console.log("CIDADE: ", cidade)
     // 1. Buscar a cidade e obter latitude e longitude
     const respostaClima = await axios.get(
@@ -47,12 +53,12 @@ const buscarClima = async () => {
       `https://api.openweathermap.org/data/2.5/forecast?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`
     );
     setPrevisao(respostaPrevisao.data.list.slice(0, 5))
-   
     console.log("PREVISÃO: ", previsao)
-
-    
+  
   } catch (error) {
     console.error("Erro ao buscar clima:", error);
+  } finally {
+    setLoading(false); // Fim do carregamento
   }
 };
 
@@ -60,6 +66,7 @@ const buscarClima = async () => {
     <div className="app">
         <h1>Condições Climáticas</h1>
         <Busca cidade={cidade} setCidade={setCidade} buscarClima={buscarClima}/>
+        {loading && <Spinner />}
         {clima && <ClimaAtual clima={clima}/>}
         {previsao.length > 0 && <Previsao previsoes={previsao}/>}
         <Footer/>
